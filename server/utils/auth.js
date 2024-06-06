6077b9402421d931bb6f11f849de27bf9c4d69af
pandord24 <gordonkwan@msn.com>
2024-06-05 21:55:27 -0700

    login and signup working

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,21 +1,40 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const secret = process.env.JWT_SECRET;
+require("dotenv").config()
+const secret = process.env.SECRET;
 const expiration = '2h';
 
 module.exports = {
-  signToken: function({ username, email, _id }) {
-    const payload = { username, email, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-  verifyToken: function(token) {
-    try {
-      return jwt.verify(token, secret);
-    } catch (err) {
-      console.log('Invalid token');
-      return null;
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
     }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
