ea306322445f6a1bb8c75f28b44fbcb8c53afde2
pandord24 <gordonkwan@msn.com>
2024-06-06 21:57:04 -0700

    we protected all resolvers with context and fixed client side login and navigation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,16 +4,57 @@ const { User, Trip, Itinerary, Activity, Expense } = require('../models');
 
 const resolvers = {
   Query: {
-    users: async () => await User.find({}),
-    user: async (_, { id }) => await User.findById(id),
-    trips: async () => await Trip.find({}),
-    trip: async (_, { id }) => await Trip.findById(id),
-    itineraries: async (_, { tripId }) => await Itinerary.find({ trip: tripId }),
-    itinerary: async (_, { id }) => await Itinerary.findById(id),
-    activities: async (_, { itineraryId }) => await Activity.find({ itinerary: itineraryId }),
-    activity: async (_, { id }) => await Activity.findById(id),
-    expenses: async (_, { tripId }) => await Expense.find({ trip: tripId }),
-    expense: async (_, { id }) => await Expense.findById(id),
+    users: async (_, __, context) => {
+      if (context.user)
+        return await User.find({})
+      throw AuthenticationError
+    },
+    user: async (_, { id }, context) => {
+      if (context.user)
+        return await User.findById(id)
+      throw AuthenticationError
+    },
+    trips: async (_, __, context) => {
+      if (context.user)
+        return await Trip.find({})
+      throw AuthenticationError
+    },
+    trip: async (_, { id }, context) => {
+      if (context.user)
+        return await Trip.findById(id)
+      throw AuthenticationError
+    },
+    itineraries: async (_, { tripId }, context) => {
+      if (context.user)
+        return await Itinerary.find({ trip: tripId })
+      throw AuthenticationError
+    },
+    itinerary: async (_, { id }, context) => {
+      if (context.user)
+        return await Itinerary.findById(id)
+      throw AuthenticationError
+    },
+    activities: async (_, { itineraryId }, context) => {
+      if (context.user)
+        return await Activity.find({ itinerary: itineraryId })
+      throw AuthenticationError
+    },
+    activity: async (_, { id }, context) => {
+      if (context.user)
+        return await Activity.findById(id)
+      throw AuthenticationError
+
+    },
+    expenses: async (_, { tripId }, context) => {
+      if (context.user)
+        return await Expense.find({ trip: tripId })
+      throw AuthenticationError
+    },
+    expense: async (_, { id }, context) => {
+      if (context.user)
+        return await Expense.findById(id)
+      throw AuthenticationError
+    },
   },
   Mutation: {
     addUser: async (_, { username, email, password }) => {
@@ -25,37 +66,49 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw  AuthenticationError;
+        throw AuthenticationError;
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw  AuthenticationError;
+        throw AuthenticationError;
       }
 
       const token = signToken(user);
       return { token, user };
     },
-    addTrip: async (_, { userId, title, description, startDate, endDate }) => {
-      const trip = await Trip.create({ title, description, startDate, endDate, user: userId });
-      await User.findByIdAndUpdate(userId, { $push: { trips: trip.id } });
-      return trip;
+    addTrip: async (_, { userId, title, description, startDate, endDate }, context) => {
+      if (context.user) {
+        const trip = await Trip.create({ title, description, startDate, endDate, user: userId });
+        await User.findByIdAndUpdate(userId, { $push: { trips: trip.id } });
+        return trip
+      };
+      throw AuthenticationError
     },
-    addItinerary: async (_, { tripId, date }) => {
-      const itinerary = await Itinerary.create({ trip: tripId, date });
-      await Trip.findByIdAndUpdate(tripId, { $push: { itineraries: itinerary.id } });
-      return itinerary;
+    addItinerary: async (_, { tripId, date }, context) => {
+      if (context.user) {
+        const itinerary = await Itinerary.create({ trip: tripId, date });
+        await Trip.findByIdAndUpdate(tripId, { $push: { itineraries: itinerary.id } });
+        return itinerary
+      };
+      throw AuthenticationError
     },
-    addActivity: async (_, { itineraryId, title, description, location, startTime, endTime }) => {
-      const activity = await Activity.create({ itinerary: itineraryId, title, description, location, startTime, endTime });
-      await Itinerary.findByIdAndUpdate(itineraryId, { $push: { activities: activity.id } });
-      return activity;
+    addActivity: async (_, { itineraryId, title, description, location, startTime, endTime }, context) => {
+      if (context.user) {
+        const activity = await Activity.create({ itinerary: itineraryId, title, description, location, startTime, endTime });
+        await Itinerary.findByIdAndUpdate(itineraryId, { $push: { activities: activity.id } });
+        return activity
+      };
+      throw AuthenticationError
     },
-    addExpense: async (_, { tripId, description, amount, date, category }) => {
-      const expense = await Expense.create({ trip: tripId, description, amount, date, category });
-      await Trip.findByIdAndUpdate(tripId, { $push: { expenses: expense.id } });
-      return expense;
+    addExpense: async (_, { tripId, description, amount, date, category }, context) => {
+      if (context.user) {
+        const expense = await Expense.create({ trip: tripId, description, amount, date, category });
+        await Trip.findByIdAndUpdate(tripId, { $push: { expenses: expense.id } });
+        return expense
+      };
+      throw AuthenticationError
     },
   },
 };
